e1aa5fd81106a0b80f7a01367fdcaa23e28c7712
Luke <lbeaumont50@msn.com>
2023-08-31 10:21:58 +0100

    Add function to submit btn

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ let myLibrary = [];
 const newBtn = document.querySelector(".new-btn");
 newBtn.addEventListener("click", function () {
   let newBookForm = document.querySelector("#new-book-form");
+
   newBookForm.style.display = "block";
 });
 
@@ -12,4 +13,17 @@ function book(title, author, pages, read) {
   this.pages = pages;
   this.read = read;
 }
-function addBookToLibrary() {}
+function addBookToLibrary() {
+  let title = document.querySelector("#title").value;
+  let author = document.querySelector("#author").value;
+  let pages = document.querySelector("#pages").value;
+  let read = document.querySelector("#read").checked;
+  let newBook = new book(title, author, pages, read);
+  console.log(newBook);
+}
+document
+  .querySelector("#new-book-form")
+  .addEventListener("submit", function () {
+    event.preventDefault();
+    alert("hello");
+  });
