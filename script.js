87d03f5dc8eb01c86cd0e511dd2b7c284fada842
Luke <lbeaumont50@msn.com>
2023-08-31 09:42:58 +0100

    creat book constructor

--- script.js
+++ script.js
@@ -5,3 +5,11 @@ newBtn.addEventListener("click", function () {
   let newBookForm = document.querySelector("#new-book-form");
   newBookForm.style.display = "block";
 });
+
+function book(title, author, pages, read) {
+  this.title = title;
+  this.author = author;
+  this.pages = pages;
+  this.read = read;
+}
+function addBookToLibrary() {}
